1efb4163fa97e6874c731413c28ca6dce3ec58a7
lihikravel <lihi.kravel@runi.ac.il>
2023-12-27 15:44:36 +0200

    change

--- Reverse.java
+++ Reverse.java
@@ -4,13 +4,15 @@ public class Reverse {
 		String s = (args[0]);
 		int i =0;
 		String sOut= " ";
-		for (i=s.length()-1; i>=0; i--){
+		for (i = s.length()-1; i>=0; i--){
 		char c = s.charAt(i);
 		sOut =  sOut + c;
-        if (i == (s.length()-1/2)){
-        System.out.print("The middle character is " + c);
-       } 
      }
 		 System.out.println(sOut);
+		 for (i = 0; i < s.length();i++){
+		 	if (i == ((s.length()-1)/2)){
+		 	System.out.println("The middle character is " +s.charAt(i));	
+		 	}
+		 }
 	}
 }
