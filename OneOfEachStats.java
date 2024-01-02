907e110d5cee9ed52671b9bcff43856aa40057c6
lihikravel <lihi.kravel@runi.ac.il>
2024-01-02 14:53:15 +0200

    change

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -67,45 +67,49 @@ public class OneOfEachStats {
 	    int kid3 = 0;
 	    int kid4 = 0;
 	    int sum = 0;
+	    double sumAll= 0;
 	    for(int i = 0; i<t; i++){
-         while (boys == 0 || girl == 0){
-		  double num = generator.nextDouble();
-		  if (num <= 0.5){
-           boys ++;
+
+        while (boys == 0 || girl == 0){
+		    double num = generator.nextDouble();
+		    if (num <= 0.5){
+                boys ++;
 		  } else {
-		    girl ++; 
+		        girl ++; 
 	      }
-	      if (girl >=1 && boys >= 1){
-	       sum = boys + girl;	
+	      if (girl >= 1 && boys >= 1){
+	          sum = boys + girl;	
 	      } 
-	      boys = 0;
-	        girl = 0;
 	   }
+	       sumAll = sumAll + sum;
 	      if (sum == 2){
              kid2++;
 	       }	
 	       if (sum == 3){
-            kid3++;
+               kid3++;
 	       }	
 	       if (sum >= 4){
-            kid4++;
+               kid4++;
 	       }
-          }
-            double ageSum = kid2*2 + kid3*3 + kid4*4;
-	        double avr = ageSum/t;
+	        boys = 0;
+	        girl = 0;
+        }
+	        double avr = sumAll/t;
 
-	        System.out.println("Average: " + avr + " children to get at least one of each gender." );
+	        System.out.println("Average: " + avr + " children to get at least one of each gender.");
 	        System.out.println("Number of families with 2 children: " + kid2);
 	        System.out.println("Number of families with 3 children: " + kid3);
 	        System.out.println("Number of families with 4 or more children: " + kid4);	
 
 	        if (kid2 > kid3 && kid2 > kid4) {
-             System.out.println("The most common number of children is " + 2 + ".");
+                System.out.println("The most common number of children is " + 2 + ".");
 
-	        } else if (kid3 > kid2 && kid3 > kid4) {
-	           System.out.println("The most common number of children is " + 3 + ".");
+	        } 
+	        else 
+	            if (kid3 > kid2 && kid3 > kid4) {
+	                System.out.println("The most common number of children is " + 3 + ".");
 
-	        } else 
-	           System.out.println("The most common number of children is 4 or more.");
+	        }   else 
+	               System.out.println("The most common number of children is 4 or more.");
            }
          }
