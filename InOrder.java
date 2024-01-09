e3b23473eafd78b08102e531f923121931decb26
runiAviv <155253539+runiAviv@users.noreply.github.com>
2024-01-09 21:21:00 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -1,14 +1,15 @@
 
 public class InOrder {
 	public static void main (String[] args) {
-		int num =(int) (10* Math.random()) ;
-		int j = -1;
-		while(num>j){
-		    System.out.print(num + " ");
-		    j =num;
-		    num = (int) (10 * Math.random());
-	    
-	 }	
+		// #feedback: use better names
+		int next =(int) (10* Math.random()) ;
+		int last;
+		// #feedback: you could do it better with do-while loop and without intializing 'last'
+		do {
+		    System.out.print(next + " ");
+		    last = next;
+		    next = (int) (10 * Math.random());
+		} while(next > last);	
 
        
 	}
