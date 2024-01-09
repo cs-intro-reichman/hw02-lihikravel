015ba572d77de16fca115ef48cb0c7d60caade42
runiAviv <155253539+runiAviv@users.noreply.github.com>
2024-01-09 21:18:17 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,18 +1,23 @@
 
 public class DamkaBoard {
 	public static void main(String[] args) {
+		// #feedback: your indentations make the code extremly hard to read...
+		
 		int num = Integer.parseInt(args[0]);
-		for (int i=0; i<num; i++){
-			if(i%2 == 0 || i==0 ){
-			   for(int j=0; j<num; j++){
-				System.out.print("* "); 
+		for (int i=0; i<num; i++) {
+			/* 
+   				#feedback: 
+       				When i=0 it yield that i % n = 0 for any n
+	   			However, it's not a good idea to split same loop for both cases.
+			*/ 
+			String pattern = " *";
+			if (i%2 == 0 ) {
+				pattern = "* ";
 			}
-		}
-		else 
-	    for(int j=0; j<num; j++){
-		    System.out.print(" *"); 
+			for(int j=0; j<num; j++) {
+				System.out.print(pattern); 
 			}
-		System.out.println();
+			System.out.println();
 		}
 	}
 }
